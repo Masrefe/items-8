36580deea6d8a003d1eb65fcf8e44c52889724b5
Ashraful Alom <masrafe2121@gmail.com>
2019-07-14 16:18:37 +0600

    html

---
+++ assets/js/active.js
@@ -0,0 +1,15 @@
+(function ($) {
+	"use strict";
+    jQuery(document).ready(function($){
+        /* meanmenu */
+        $('.main-menu').meanmenu({
+              meanMenuContainer: '.mobile-menu',
+              meanScreenWidth: "991"
+          }); 
+     });
+
+   
+
+}(jQuery));	
+
+
